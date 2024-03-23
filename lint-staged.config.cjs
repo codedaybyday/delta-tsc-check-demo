d95c698b42e1a42f0111348737b64ec99d82f556
liubeijing <liubeijing@meituan.com>
2024-03-23 17:14:43 +0800

    docs: ✏️ add api invoke

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,3 +1,14 @@
+const path = require('path');
+const { performMultiTSCheck } = require('delta-tsc-check');
+
 module.exports = {
-    '**/*.ts': ['npx tsc-check --files']
+    '**/*.ts': ['npx tsc-check --files'],
+    '**/*.tsx': async (filenames) => {
+        // Generate tsc-related execution commands
+        const { commands = [] } = await performMultiTSCheck({ filenames, lintstaged: true });
+        // Other commands such as eslint
+        // commands.push(`prettier ${filenames.join(' ')} --write`);
+        // commands.push(`eslint --ignore-path ${eslintignorePath} ${filenames.join(' ')} --fix --quiet --cache`);
+        return commands;
+    },
 }
